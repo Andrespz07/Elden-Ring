fac190fd67eabed74997850fb1a7e754b8563f41
Andrespz07 <andresperezortega2001@gmail.com>
2023-03-18 16:47:47 +0100

    miscellaneous resources added to the repository

---
+++ src/main/java/com/eldenringdb/eldenringsp/EldenringspApplication.java
@@ -0,0 +1,13 @@
+package com.eldenringdb.eldenringsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EldenringspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EldenringspApplication.class, args);
+	}
+
+}
