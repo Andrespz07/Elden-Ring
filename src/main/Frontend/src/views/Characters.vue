da0f1b25b65d778466269bc6c25a960139fc0d28
Andrespz07 <andresperezortega2001@gmail.com>
2023-04-09 22:34:45 +0200

    CreateCharacterForm

--- src/main/Frontend/src/views/Characters.vue
+++ src/main/Frontend/src/views/Characters.vue
@@ -6,6 +6,7 @@ import CardCharacter from '../components/CardCharacter.vue';
 <template>
 <section id="background">
 <Header></Header>
+<h1>Characters</h1>
  <main id="cardsContainer">
 <CardCharacter></CardCharacter>
  </main>
@@ -21,8 +22,19 @@ import CardCharacter from '../components/CardCharacter.vue';
         background-image: url(../assets/img/secBackground.png);
         background-size: cover;
         // background-position: center; al quitar esto, la imagen se adapta mejor pero solo se ve la parte superior, quizá con más contenido se arregle
-        height: 100vh;
+        // height: 100vh;
         width: auto;
+        h1{
+            color: $Golden;
+            text-align: center;
+            font-size: 3.5rem;
+            font-weight: 100;
+            text-transform: uppercase;
+            display: flex;
+            justify-content: center;
+            margin-top: 6vh;
+            margin-bottom: 10vh;
+        }
     }
 }
 </style>
---
+++ src/main/Frontend/src/views/CreateCharacter.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import CreateCharacterForm from '../components/CreateCharacterForm.vue';
+
+</script>
+
+<template>
+    <div id="background">
+<CreateCharacterForm></CreateCharacterForm>
+</div>
+</template>
+<style lang="scss" scoped>
+@import '../assets/scss/variables.scss';
+
+#background{
+        background-image: url(../assets/img/secBackground.png);
+        background-size: cover;
+        // background-position: center; al quitar esto, la imagen se adapta mejor pero solo se ve la parte superior, quizá con más contenido se arregle
+        height: 100vh;
+        width: auto;
+    }
+    
+</style>
