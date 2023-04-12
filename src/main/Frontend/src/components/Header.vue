00aaef2e7f64b9017e6acd62459719e61bd6b54e
Andrespz07 <andresperezortega2001@gmail.com>
2023-04-12 19:55:44 +0200

    header of all views with logo

--- src/main/Frontend/src/components/Header.vue
+++ src/main/Frontend/src/components/Header.vue
@@ -6,6 +6,11 @@ import YourCharactersButton from '../components/YourCharactersButton.vue'
 
 <template>
     <header>
+      <div id="logo">
+        <router-link to="/">
+        <img src="../components/icons/isotype.png" alt="Logo Elden Ring">
+      </router-link>
+      </div>
         <v-breadcrumbs id="components">
     <v-breadcrumbs-item class="buttons">
       <YourCharactersButton  />
@@ -20,9 +25,26 @@ import YourCharactersButton from '../components/YourCharactersButton.vue'
 <style lang="scss" scoped>
 @import '../assets/scss/variables.scss';
 
+header{
+  display: flex;
+  width: 100vw;
+  height: 30vh;
+  justify-content: space-between;
+#logo{
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 4vh;
+  margin-left: 3vw;
+  img{
+    width: auto;
+    height: 15vh;
+  }
+}
+
 #components{
     display: flex;
     justify-content: flex-end;
+    margin: -4% 3% 0 0;
 }
-
+}
 </style>
