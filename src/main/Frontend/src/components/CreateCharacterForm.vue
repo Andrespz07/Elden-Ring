723fbbd74ce465331300699da4eebe986a83f94e
Andrespz07 <andresperezortega2001@gmail.com>
2023-04-17 23:36:36 +0200

    improved form

--- src/main/Frontend/src/components/CreateCharacterForm.vue
+++ src/main/Frontend/src/components/CreateCharacterForm.vue
@@ -2,86 +2,115 @@
 import { RouterLink } from 'vue-router';
 import { reactive, ref } from 'vue';
 
-// const items = ['Astrologer', 'Bandit', 'Confessor', 'Hero', 'Prisoner', 'Prophet', 'Samurai', 'Vagabond', 'Warrior', 'Wretch'];
-
-// const data = () => ({
-//   select: null,
-//   items
-// })
-
 const items = ['Astrologer', 'Bandit', 'Confessor', 'Hero', 'Prisoner', 'Prophet', 'Samurai', 'Vagabond', 'Warrior', 'Wretch'];
-const data = () => ({
+
+const form = reactive({
   name: '',
   affiliation: '',
   race: '',
   role: '',
-  select: '',
-  items
+  selectedClass: '',
+  items,
+  errors: {}
 })
 
 const select = ref('');
+
+const requiredFields = {
+  name: 'Name is required',
+  affiliation: 'Affiliation is required',
+  race: 'Race is required',
+  role: 'Role is required',
+  selectedClass: 'Class is required'
+}
+
+function validateForm() {
+  form.errors = {};
+
+  for (const field of Object.keys(requiredFields)) {
+    if (!form[field]) {
+      form.errors[field] = requiredFields[field];
+    }
+  }
+
+  return Object.keys(form.errors).length === 0;
+}
+
+function submit() {
+  if (validateForm()) {
+    // submit form data here
+  }
+}
+
+function handleReset() {
+  Object.keys(form).forEach(key => {
+    form[key] = '';
+  });
+  form.errors = {};
+}
+
 </script>
-
 <template>
-    
-<div class="form-background ">
-  <v-form class="form " @submit.prevent="submit">
+  <div class="form-background">
+    <v-form class="form" @submit.prevent="submit">
+      <v-label class="label">Name</v-label>
+      <v-text-field
+        v-model="form.name"
+        :error-messages="form.errors.name"
+        class="input"
+        required
+      ></v-text-field>
+      <v-label class="label">Affiliation</v-label>
+  <v-text-field
+    v-model="form.affiliation"
+    :error-messages="form.errors.affiliation"
+    class="input"
+    required
+  ></v-text-field>
 
-    <v-label class="label">Name</v-label>
-    <v-text-field
-      v-model="name"
-      :error-messages="name"
-      class="input"
-      required
-    ></v-text-field>
+  <v-label class="label">Race</v-label>
+  <v-text-field
+    v-model="form.race"
+    :error-messages="form.errors.race"
+    class="input"
+    required
+  ></v-text-field>
 
-    <v-label class="label">Affilliation</v-label>
-    <v-text-field
-      v-model="afilliation"
-      :error-messages="phone"
-      class="input"
-    ></v-text-field>
+  <v-label class="label">Role</v-label>
+  <v-text-field
+    v-model="form.role"
+    :error-messages="form.errors.role"
+    class="input"
+    required
+  ></v-text-field>
 
-    <v-label class="label">Race</v-label>
-    <v-text-field
-      v-model="race"
-      :error-messages="email"
-      class="input"
-    ></v-text-field>
+  <v-label class="label">Class</v-label>
+  <v-select
+    v-model="form.selectedClass"
+    :items="form.items"
+    :error-messages="form.errors.selectedClass"
+    class="input"
+    required
+  ></v-select>
 
-    <v-label class="label">Role</v-label>
-    <v-text-field
-      v-model="role"
-      :error-messages="email"
-      class="input"
-    ></v-text-field>
+  <v-btn
+    class="me-4 buttons"
+    type="submit"
+    variant="text"
+  >
+    Submit
+  </v-btn>
 
-    <v-label class="label">Class</v-label>
-    <v-select
-      v-model="select"
-      :items="items"
-      value="items"
-      :error-messages="select"
-      class="input"
-    ></v-select>
-
-    <v-btn
-      class="me-4 buttons"
-      type="submit"
-      variant="text"
-    >
-      Send
-    </v-btn>
-    
-    <router-link to="Characters">
+  <router-link to="Characters">
     <v-btn @click="handleReset" class="buttons" variant="text">
       Cancel
     </v-btn>
-    </router-link>
-  
-  </v-form>
+  </router-link>
+
+</v-form>
 </div>
 </template>
+
 <style lang="scss" scoped>
 @import '../assets/scss/variables.scss';
     
